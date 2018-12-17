214d5c012cc5ba99eee8aa6330affad15b80be87
刘庚新 <lgx@meitu.com>
2018-12-17 21:35:43 +0800

    modify token

---
+++ bin/token/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/meitu/token"
+)
+
+var (
+	key     string
+	auth    string
+	payload string
+)
+
+var Usage = func() {
+	fmt.Println("USAGE: ./token -key=key,-auth=auth,-payload=payload")
+	fmt.Println("\nThe commond are:\n\t-key server key \n\t-auth client auth 。\n\t-payload service payload。")
+}
+
+func main() {
+
+	flag.StringVar(&key, "key", "", "server key")
+	flag.StringVar(&auth, "auth", "", "client auth")
+	flag.StringVar(&payload, "payload", "", "server payload")
+	flag.Parse()
+	if key == "" {
+		fmt.Println("serverkey is empty,Please enter serverkey on the command line")
+		return
+	}
+	tt := token.New([]byte(key))
+	if auth != "" && payload == "" {
+		res, err := tt.Verify([]byte(auth))
+		if err != nil {
+			fmt.Printf("Parse failed :%s\n", err)
+			return
+		}
+		if res {
+			fmt.Println("Prase sucess")
+		} else {
+			fmt.Printf("Prase faild")
+
+		}
+		return
+	} else if auth == "" && payload != "" {
+		//Token is generated based on serverkey and namespace
+		auth, err := tt.Sign([]byte(payload))
+		if err != nil {
+			fmt.Printf("Create auth failed %s\n", err)
+			return
+		}
+		fmt.Printf("auth : %s\n", auth)
+		return
+	} else {
+		Usage()
+	}
+	return
+}
---
+++ token.go
@@ -0,0 +1,91 @@
+package token
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"strconv"
+	"time"
+)
+
+const (
+	CurrentVesion = 1
+	tokenSignLen  = 11
+)
+
+type Token struct {
+	version int32
+	key     []byte
+}
+
+func New(key []byte) *Token {
+	return &Token{version: CurrentVesion, key: key}
+}
+
+func (t *Token) Sign(data []byte) ([]byte, error) {
+	m := &message{version: CurrentVesion, createAt: int64(time.Now().Unix()), payload: data}
+	data, err := m.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	mac := hmac.New(sha256.New, t.key)
+	mac.Write(data)
+	sign := mac.Sum(nil)
+
+	//truncate to 32 byte: https://tools.ietf.org/html/rfc2104#section-5
+	// we have 11 byte rigth of hmac,so the rest of data is token message
+	sign = sign[:tokenSignLen]
+
+	encodedSign := make([]byte, hex.EncodedLen(len(sign)))
+	hex.Encode(encodedSign, sign)
+	var token []byte
+	token = append(token, data...)
+	token = append(token, '-')
+	token = append(token, encodedSign...)
+	return token, nil
+}
+
+//Verify token auth
+func (t *Token) Verify(sign []byte) (bool, error) {
+	encodedSignLen := hex.EncodedLen(tokenSignLen)
+	if len(sign) < encodedSignLen || len(t.key) == 0 {
+		return false, errors.New("token or key is parameter illegal")
+	}
+
+	s := make([]byte, tokenSignLen)
+	hex.Decode(s, sign[len(sign)-encodedSignLen:])
+
+	meta := sign[:len(sign)-encodedSignLen-1] //counting in the ":"
+	mac := hmac.New(sha256.New, t.key)
+	mac.Write(meta)
+
+	if !hmac.Equal(mac.Sum(nil)[:tokenSignLen], s) {
+		return false, errors.New("token mismatch")
+	}
+
+	var m message
+
+	if err := m.UnmarshalBinary(meta); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+type message struct {
+	version  int64
+	createAt int64
+	payload  []byte
+}
+
+//MarshalBinary Namespace SHOULD NOT contains a colon
+func (m *message) MarshalBinary() (data []byte, err error) {
+	data = append(data, m.payload...)
+	data = append(data, '-')
+	data = append(data, []byte(strconv.FormatInt(m.createAt, 10))...)
+	data = append(data, '-')
+	data = append(data, []byte(strconv.FormatInt(m.version, 10))...)
+	return data, nil
+}
