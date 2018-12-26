8500b3d263877ae043daea9b18277dc2933f2d5e
刘庚新 <lgx@meitu.com>
2018-12-26 15:32:57 +0800

    add Auth

--- token.go
+++ token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
@@ -70,6 +71,32 @@ func (t *Token) Verify(sign []byte) error {
 	return nil
 }
 
+// Auth a token and renturn payload
+func (t *Token) Auth(sign []byte) ([]byte, error) {
+	encodedSignLen := hex.EncodedLen(tokenSignLen)
+	if len(sign) < encodedSignLen {
+		return nil, errors.New("auth invalid size")
+	}
+
+	s := make([]byte, tokenSignLen)
+	hex.Decode(s, sign[len(sign)-encodedSignLen:])
+
+	meta := sign[:len(sign)-encodedSignLen-1] //counting in the ":"
+	mac := hmac.New(sha256.New, t.key)
+	mac.Write(meta)
+
+	if !hmac.Equal(mac.Sum(nil)[:tokenSignLen], s) {
+		return nil, errors.New("token mismatch")
+	}
+
+	var m message
+
+	if err := m.UnmarshalBinary(meta); err != nil {
+		return nil, err
+	}
+	return m.payload, nil
+}
+
 // message contains the necessary constituent fields for a signature
 type message struct {
 	version  int64
@@ -85,4 +112,28 @@ func (m *message) MarshalBinary() (data []byte, err error) {
 	data = append(data, '-')
 	data = append(data, []byte(strconv.FormatInt(m.version, 10))...)
 	return data, nil
+}
+
+func (m *message) UnmarshalBinary(data []byte) error {
+	fields := bytes.Split(data, []byte{'-'})
+	l := len(fields)
+	if l < 3 {
+		return errors.New("invalid token")
+	}
+
+	version, err := strconv.ParseInt(string(fields[l-1]), 10, 64)
+	if err != nil {
+		return err
+	}
+	m.version = version
+
+	createAt, err := strconv.ParseInt(string(fields[l-2]), 10, 64)
+	if err != nil {
+		return err
+	}
+	m.createAt = createAt
+
+	m.payload = bytes.Join(fields[:l-2], []byte(""))
+
+	return nil
 }
